e98ceb918a91c9a2af5f126e60d1ee866c79749e
Kieran Klukas <kieran@dunkirk.sh>
2026-06-01 15:15:24 -0400

    Initial Commit

---
+++ cxp-testbed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cxp-testbed
+//
+//  Created by Kieran Klukas on 6/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cxp-testbed/cxp_testbedApp.swift
@@ -0,0 +1,17 @@
+//
+//  cxp_testbedApp.swift
+//  cxp-testbed
+//
+//  Created by Kieran Klukas on 6/1/26.
+//
+
+import SwiftUI
+
+@main
+struct cxp_testbedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
